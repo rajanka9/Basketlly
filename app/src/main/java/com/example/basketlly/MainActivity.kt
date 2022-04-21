0a81e89f6c38da5bcac584eddb75cee69ca5afbf
rajanka9 <jana9radikova@gmail.com>
2022-04-21 16:49:26 +0200

    Farbicky

--- app/src/main/java/com/example/basketlly/MainActivity.kt
+++ app/src/main/java/com/example/basketlly/MainActivity.kt
@@ -3,9 +3,11 @@ package com.example.basketlly
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//testovacie veci
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
