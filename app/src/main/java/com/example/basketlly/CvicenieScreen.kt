283167b034d8f08391ed1487274d86fd472ee83b
rajanka9 <jana9radikova@gmail.com>
2022-06-13 20:04:02 +0200

    toasty a pridanie zdrojov + mazanie commentov

--- app/src/main/java/com/example/basketlly/CvicenieScreen.kt
+++ app/src/main/java/com/example/basketlly/CvicenieScreen.kt
@@ -1,12 +1,11 @@
 package com.example.basketlly
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.basketlly.databinding.CvicenieScreenBinding
 
-//class CvicenieScreenAktivita : Fragment(R.layout.cvicenie_screen){
-
-// https://www.youtube.com/watch?v=hjEE6c0oOI0
+// Zdroje: app ktora nam vypocita percento z "tipu" z 4teho cicenia
 class CvicenieScreen: AppCompatActivity() {
 
     private lateinit var binding: CvicenieScreenBinding
@@ -26,17 +25,22 @@ class CvicenieScreen: AppCompatActivity() {
     private fun percentoStrelby() {
         val nazovTextPole = binding.nameExercise.text
         val daneTextPole = binding.numberSuccessfulAttempts.text.toString()
-            //binding.numberSuccessfulAttempts.text.toString()
         val pokusyTextPole = binding.numberAllAttempts.text.toString()
-            //binding.nameAllAttempts.text.toString()
-
 
         val daneHodnota = daneTextPole.toIntOrNull()
         val pokusyHodnota = pokusyTextPole.toIntOrNull()
 
-        if(daneHodnota == null || pokusyHodnota == null || pokusyHodnota == 0) {
+        if(daneHodnota == null || pokusyHodnota == null || pokusyHodnota == 0 || daneHodnota>pokusyHodnota) {
             binding.percentageOfExercise.text = ""
+            if (pokusyHodnota == 0)
+            {val toast = Toast.makeText(this, "Division by zero!", Toast.LENGTH_SHORT)
+            toast.show()}
+
+            if (daneHodnota!! > pokusyHodnota!! && pokusyHodnota != 0)
+            {val toast = Toast.makeText(this, "succesful attempts > all attempts --> mistake!", Toast.LENGTH_SHORT)
+                toast.show()}
             return
+
         }
 
         if (daneHodnota == null || pokusyHodnota == null || daneHodnota == 0 )
@@ -44,22 +48,6 @@ class CvicenieScreen: AppCompatActivity() {
             displayPercento(0)
             return
         }
-        /*val daneHodnota = daneTextPole.toInt()
-        val pokusyHodnota = pokusyTextPole.toInt()
-
-
-        if( daneTextPole == null && pokusyTextPole == null ) {
-            binding.percentageOfExercise.text.apply {
-                " "
-            }
-            return
-        }*/
-
-        //displayPercento(daneTextPole.toInt(), pokusyTextPole.toInt())
-        //if (daneHodnota >= 0 && pokusyHodnota > 0 && daneHodnota < pokusyHodnota  ) {
-            //var dataCvicenie = DataCvicenie(nazovTextPole.toString(), daneTextPole, pokusyTextPole)
-            //displayPercento(daneTextPole,pokusyTextPole)
-        //}
 
         val percento = (daneHodnota.toDouble() / pokusyHodnota * 100).toInt()
         displayPercento(percento)
@@ -68,7 +56,6 @@ class CvicenieScreen: AppCompatActivity() {
     private fun displayPercento(percento: Int) {
         //val formattedPercento = NumberFormat.getCurrencyInstance().format(percento)
         binding.percentageOfExercise.text = getString(R.string.percentage, percento.toString()+"%")
-
     }
 
 }
